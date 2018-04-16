cd9b42c370bd1689fc5a3e27e6a7041c1934a8b9
muramasah <hitokiri.muramasa@gmail.com>
2018-04-16 16:05:42 -0300

    Modified TrendingSection component, deleted TrendingGifGallery

--- src/component/section/TrendingSection.js
+++ src/component/section/TrendingSection.js
@@ -1,15 +1,16 @@
 //Libraries
 import React from 'react';
 //Components
-import TrendingGifGallery from '../gallery/TrendingGifGallery';
+import GifGallery from '../gallery/GifGallery';
+import TrendingImageItem from '../gallery/image/TrendingImageItem'
 
 /**
  * TrendingGifGallery
  * @param {object} props Properties of the component
  * @param {integer} props.limit Gifs request limit
  */
-const TrendingSection = ({ limit }) => {
-    return <TrendingGifGallery title='Trending' limit={limit} />
+const TrendingSection = ({ gifs }) => {
+    return <GifGallery title='Trending' gifs={gifs} imageView={TrendingImageItem} />
 };
 
 export default TrendingSection;
